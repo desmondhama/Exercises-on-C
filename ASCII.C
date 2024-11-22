89f1e743c04a1db1f7b1656a90eff77d6498e2b3
desmondhama <desmondhama@gmail.com>
2024-11-22 18:40:13 +0000

    created: new files

--- ASCII.C
+++ ASCII.C
@@ -3,9 +3,17 @@
 int main() {
 
   int asciiChar;
+  int space = 32;
   printf("Enter a value between 33-106 for its ASCII value:");
   scanf(" %d", &asciiChar);
 
-  printf("ASCII --> CHARACTER: %d --> %c\n", asciiChar, asciiChar);
+  printf("%d",space);
+  printf("ASCII --> CHARACTER: %d%c--> %c\n", asciiChar, space, asciiChar);
   return 0;
+}
+int Vidgoplay(void) {
+  int numTest;
+
+  printf("Enter numtest: ");
+  scanf("%d",&numTest);
 }
---
+++ squere_root_sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int i = 1;
+    while (i <= 10) {
+  
+    int *pnumum = 1;
+     num = num+=;
+    
+    printf("%d X\t\n",num);
+    i++;
+  }
+}
